107fb16886c4ed7d9794fe196982f061cec1d4dd
Palak <palakamin2206@gmail.com>
2020-03-28 19:20:44 +0530

    scrapping

--- Web Scrapping/temp.py
+++ Web Scrapping/temp.py
@@ -6,10 +6,10 @@ uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
 page_soup = soup(page_html, "html.parser")
-commetns = page_soup.find_all(attrs={"class":"location-review-review-list-parts-ExpandableReview__reviewText--gOmRC"})
+comments = page_soup.find_all(attrs={"class":"location-review-review-list-parts-ExpandableReview__reviewText--gOmRC"})
 users = page_soup.find_all(attrs={"class":"ui_header_link social-member-event-MemberEventOnObjectBlock__member--35-jC"})
 
 # print(containers)
-for x in commetns:
+for x in comments:
     print(x.span.get_text())
 # print()
